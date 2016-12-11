da342cd4bafeb05ffbd294601481ab0150d4624d
vzhukovskyy <vadym.zhukovskyy@gmail.com>
2016-12-11 11:38:30 +0200

    2nd switch added

---
+++ hardware.js
@@ -0,0 +1,17 @@
+module.exports = {    
+    write: function(index, state) {
+        if(0 <= index && index <= 1) {
+            io[index].write(state ? 1 : 0);
+        }
+    },
+    
+    read: function() {
+        return [io[0].read(), io[1].read()];
+    },
+};
+
+var mraa = require("mraa");
+
+var io = [new mraa.Gpio(11, true, false), new mraa.Gpio(12, true, false)];
+io[0].dir(mraa.DIR_OUT); // configure the LED gpio as an output
+io[1].dir(mraa.DIR_OUT);
